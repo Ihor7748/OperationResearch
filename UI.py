d979f392eddc44541d0094a405f9fc3bc924905c
Ihor7748 <43166074+Ihor7748@users.noreply.github.com>
2022-01-19 18:09:17 +0200

    Add files via upload

---
+++ UI.py
@@ -0,0 +1,116 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QGridLayout
+import sys
+import numpy as np
+from transport import traffic, nwa
+
+
+WIDTH = 1200
+HEIGHT = 675
+
+
+class LabelGrid(QtWidgets.QWidget):
+    def __init__(self, parent, size_x, size_y):
+        super(LabelGrid, self).__init__(parent)
+        self.size_x = size_x
+        self.size_y = size_y
+        self.labels = []
+        grid_layout = QtWidgets.QGridLayout()
+        self.setLayout(grid_layout)
+        for i in range(self.size_y):
+            self.labels.append([])
+            for j in range(self.size_x):
+                label = QtWidgets.QLabel()
+                self.labels[i].append(label)
+                grid_layout.addWidget(label, i, j)
+
+    def set_value(self, values):
+        for i in range(self.size_y):
+            for j in range(self.size_x):
+                self.labels[i][j].setText(str(values[i, j]))
+
+
+class InputGrid(QtWidgets.QWidget):
+    def __init__(self, parent, size_x, size_y):
+        super(InputGrid, self).__init__(parent)
+        self.size_x = size_x
+        self.cap_x = size_x
+        self.size_y = size_y
+        self.cap_y = size_y
+        self.entries = []
+        self.layout = QtWidgets.QGridLayout()
+        self.setLayout(self.layout)
+        for i in range(self.size_y):
+            self.entries.append([])
+            for j in range(self.size_x):
+                entry = QtWidgets.QLineEdit()
+                self.entries[i].append(entry)
+                self.layout.addWidget(entry, i, j)
+
+    def get_value(self):
+        res = []
+        for i in range(self.size_y):
+            res.append([])
+            for j in range(self.size_x):
+                tmp = self.entries[i][j].text()
+                if tmp:
+                    res[i].append(float(tmp))
+                else:
+                    res[i].append(0.0)
+        return np.array(res)
+
+
+
+                
+
+
+
+
+class Ui_MainWindow(QtWidgets.QWidget):
+    def setupUi(self, MainWindow):
+        MainWindow.resize(WIDTH, HEIGHT)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(700, 400, 200, 100))
+        self.cost_inputGrid = InputGrid(self.centralwidget, 5, 4)
+        self.production_inputGrid = InputGrid(self.centralwidget, 1, 4)
+        self.consumption_inputGrid = InputGrid(self.centralwidget, 5, 1)
+        self.outputGrid = LabelGrid(self.centralwidget, 5, 4)
+        self.cost_inputGrid.setGeometry(QtCore.QRect(10, 10, 600, 300))
+        self.production_inputGrid.setGeometry(QtCore.QRect(650, 10, 135, 300))
+        self.consumption_inputGrid.setGeometry(QtCore.QRect(10, 350, 600, 100))
+        self.outputGrid.setGeometry(QtCore.QRect(25, 450, 600, 300))
+       
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+    
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Proceed"))
+        self.pushButton.clicked.connect(self.perform)
+
+    def perform(self):
+        c = self.cost_inputGrid.get_value()
+        a = self.production_inputGrid.get_value()[:, 0]
+        b = self.consumption_inputGrid.get_value()[0]
+        d, is_base = nwa(c, a, b)
+        d, is_base, p = traffic(c, d, is_base)
+        while p.any():
+            d, is_base, p = traffic(c, d, is_base)
+        d = np.round(d, 5)
+        self.outputGrid.set_value(d)
+        
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show() 
+
+    sys.exit(app.exec_())
---
+++ transport.py
@@ -0,0 +1,164 @@
+import numpy as np
+
+# north-west angle
+def nwa(c, a, b):
+    d = np.zeros(c.shape)
+    is_base = np.zeros(c.shape, dtype=bool)
+    i, j = 0, 0
+    n = c.shape[0]
+    m = c.shape[1]
+    a_tmp = np.copy(a)
+    b_tmp = np.copy(b)
+    while i< n and j < m:
+        is_base[i, j] = True
+        if a_tmp[i] > b_tmp[j]:
+            d[i, j] = b_tmp[j]
+            a_tmp[i] -= b_tmp[j]
+            j += 1
+        else:
+            d[i, j] = a_tmp[i]
+            b_tmp[j] -= a_tmp[i]
+            i += 1
+    return d, is_base
+
+def find_beta(base, is_base, k_list, alpha, beta):
+    for k in k_list:
+        beta[is_base[k]] = base[k, is_base[k]] - alpha[k]
+        k_list_next = [k for k, j in enumerate(is_base[k]) if j]
+        is_base[k] = False
+        if k_list_next:
+            alpha, beta, is_base = find_alpha(base, is_base, k_list_next, alpha, beta)
+    return alpha, beta, is_base
+
+def find_alpha(base, is_base, k_list, alpha, beta):
+    for k in k_list:
+        alpha[is_base[:,k]] = base[is_base[:,k], k] - beta[k]
+        k_list_next = [k for k, j in enumerate(is_base[:, k]) if j]
+        is_base[:, k] = False
+        if k_list_next:
+            alpha, beta, is_base = find_beta(base, is_base, k_list_next, alpha, beta)
+    return alpha, beta, is_base
+
+
+def traffic(c, d, is_base):
+    alpha = np.zeros(c.shape[0])
+    beta = np.zeros(c.shape[1])
+    k = 0
+    while np.count_nonzero(is_base) < (c.shape[0]+c.shape[1]-1):
+        x, y = false_var_place_search(is_base)
+        d[x, y] = 0.0000001
+        is_base[x, y] = True
+    alpha, beta, _ = find_beta(c, np.copy(is_base), [k], alpha, beta)
+    d_ = np.where(abs(d - 0)<0.0000000001, alpha[:, np.newaxis]+beta[np.newaxis, :], d)
+    pivot = d_ - c > 0
+    pivot[is_base] = False
+    cycles = []
+    measures = []
+    for i in range(c.shape[0]):
+        for j in range(c.shape[1]):
+            if pivot[i,j]:
+                cycles += cycle_search([(i, j)], is_base)
+
+    for p in range(len(cycles)):
+        x = [i for (i, j) in cycles[p]]
+        y = [j for (i, j) in cycles[p]]
+        m = min(d[x[1::2], y[1::2]])
+        me = (c[x[0], y[0]] - d_[x[0], y[0]])*m
+        measures.append(me)
+    cycles_ = []
+    if cycles:
+        cycles_ = cycle_intersect(cycles, measures)
+    for i in cycles_:
+        d, is_base = use_cycle(i, d, is_base)
+    return d, is_base, pivot
+
+
+def use_cycle(cycle, d, is_base):
+    x = [i for (i, j) in cycle]
+    y = [j for (i, j) in cycle]
+    m = min(d[x[1::2], y[1::2]])
+    d[x[1::2], y[1::2]] -= m
+    d[x[0::2], y[0::2]] += m
+    is_base[x[0], y[0]] = True
+    is_base = np.where(d== 0, False, True)
+    return d, is_base
+
+
+def false_var_place_search(is_base):
+    # find first base
+    x, y = np.where(is_base)
+    bases = zip(list(x), list(y))
+    # find reachable bases
+    reachable = find_reach([(x[0], y[0])], x[0], y[0], is_base)
+    non_reach = [i for i in bases if i not in reachable]
+    return 0, non_reach[0][0]
+
+
+def cycle_intersect(cycles, measures):
+    inter = np.zeros((len(cycles), len(cycles)+1))
+    for i in range(len(cycles)):
+        for j in range(len(cycles)):
+            if [value for value in cycles[i] if value in cycles[j]]:
+                inter[i, j] = 0
+            else:
+                inter[i, j] = measures[i] + measures[j]
+        inter[i, len(cycles)] = measures[i]
+    i, j = np.where(inter == np.min(inter))
+    if j[0] == len(cycles):
+        return [cycles[i[0]]]
+    else:
+        return [cycles[i[0]], cycles[j[0]]]
+
+
+
+def find_reach(reach_list, x, y, is_base):
+    
+    new_reach_list = [(x, i) for (i, b) in enumerate(is_base[x]) if b and (x, i) not in reach_list]
+    new_reach_list += [(j, y) for (j, b) in enumerate(is_base[:, y]) if b and (j, y) not in reach_list]
+    reach_list += new_reach_list
+    for (i, j) in new_reach_list:
+        reach_list += find_reach(reach_list, i, j, is_base)
+    return reach_list
+
+
+def cycle_search(path, is_base):
+    start_x = path[-1][0]
+    start_y = path[-1][1]
+    bases = []
+    bases = []
+   
+    if len(path) > 2:
+        if path[0][0] == start_x:
+            return [path]
+        elif path[0][1] == start_y:
+            return [path]
+ 
+
+    if len(path) > 1:
+        if path[-2][0] == path[-1][0]:
+            for i in range(is_base.shape[0]):
+                if is_base[i, start_y]:
+                    bases.append((i, start_y))
+        elif path[-2][1] == path[-1][1]:
+            for j in range(is_base.shape[1]):
+                if is_base[start_x, j]:
+                    bases.append((start_x, j))
+        bases = [(i, j) for (i, j) in bases 
+                if (i, j) not in path]
+    else:
+        for i in range(is_base.shape[0]):
+            if is_base[i, start_y]:
+                bases.append((i, start_y))
+
+
+    alternatives = []
+    for b in bases:
+        path.append(b)
+        alt = cycle_search(path.copy(), is_base)
+        path.pop()
+        if alt:
+            alternatives += alt
+    return alternatives
+
+
+
